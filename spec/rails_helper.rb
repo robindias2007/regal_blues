afdf3546ec761ffb398aae9377db73cab478a031
p150 <pavanprakash21@gmail.com>
2017-08-09 12:52:41 +0530

    Install and configure Rspec and related gems like Simlpecov, Factory Girl, Database Cleaner and Shoulda Matchers

---
+++ spec/rails_helper.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../../config/environment', __FILE__)
+abort('The Rails environment is running in production mode!') if Rails.env.production?
+require 'rspec/rails'
+
+ActiveRecord::Migration.maintain_test_schema!
+  
+RSpec.configure do |config|
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.use_transactional_fixtures = true
+  config.infer_spec_type_from_file_location!
+  config.filter_rails_from_backtrace!
+
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do |suite|
+    DatabaseCleaner.strategy = suite.metadata[:js] ? :truncation : :transaction
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do |_suite|
+    DatabaseCleaner.clean
+  end
+
+  config.include FactoryGirl::Syntax::Methods
+
+  Shoulda::Matchers.configure do |configure|
+    configure.integrate do |with|
+      with.test_framework :rspec
+      with.library :rails
+    end
+  end
+end
