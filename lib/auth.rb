461e474d402fb5cbf5d6fa3f1e28ee2849698d17
p150 <pavanprakash21@gmail.com>
2017-08-11 22:50:00 +0530

    Add auth library using JWT with RSA keys

---
+++ lib/auth.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require 'jwt'
+
+class Auth
+  ALGORITHM = 'RS256'
+
+  def self.issue(payload)
+    JWT.encode(payload, rsa_private, ALGORITHM)
+  end
+
+  def self.decode(token)
+    JWT.decode(token, rsa_public, true, algorithm: ALGORITHM).first
+  end
+
+  def self.rsa_private
+    @key ||= OpenSSL::PKey::RSA.new(File.read('jwt_rsa_private.pem'))
+  end
+
+  def self.rsa_public
+    rsa_private.public_key
+  end
+end
