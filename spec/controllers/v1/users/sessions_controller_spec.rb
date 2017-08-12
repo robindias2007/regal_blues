9f0cfe23e1ed03634f95d5ae006604a80fffbe8f
p150 <pavanprakash21@gmail.com>
2017-08-12 12:07:20 +0530

    Update the specs to include username and negative specs and pass all

--- spec/controllers/v1/users/sessions_controller_spec.rb
+++ spec/controllers/v1/users/sessions_controller_spec.rb
@@ -5,14 +5,31 @@ describe V1::Users::SessionsController, type: :controller do
     let(:user) { create :user }
 
     it 'returns a http success if valid params are passed' do
-      post :create, params: { email: user.email, password: user.password }
+      post :create, params: { login: user.email, password: user.password }
       expect(response).to have_http_status 200
     end
 
-    it 'returns a valid jwt if valid params are passed' do
-      post :create, params: { email: user.email, password: user.password }
+    it 'returns a valid jwt if valid email is passed' do
+      post :create, params: { login: user.email, password: user.password }
       returned = JSON.parse(response.body)
       expect(returned).to include 'jwt'
+    end
+
+    it 'returns a valid jwt if valid username is passed' do
+      post :create, params: { login: user.username, password: user.password }
+      returned = JSON.parse(response.body)
+      expect(returned).to include 'jwt'
+    end
+
+    it 'returns a http Unauthorized if invalid params are passed' do
+      post :create, params: { login: user.email, password: 'password' }
+      expect(response).to have_http_status 401
+    end
+
+    it 'does not return a jwt if invalid params are passed' do
+      post :create, params: { login: user.email, password: 'password' }
+      returned = JSON.parse(response.body)
+      expect(returned).not_to include 'jwt'
     end
   end
 end
