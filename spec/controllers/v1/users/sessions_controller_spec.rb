853b2237dea83405e0343e4ba4d5d1c2a0978316
p150 <pavanprakash21@gmail.com>
2017-08-12 11:50:21 +0530

    Pass session controller specs for create action

--- spec/controllers/v1/users/sessions_controller_spec.rb
+++ spec/controllers/v1/users/sessions_controller_spec.rb
@@ -1,5 +1,18 @@
-require 'rails_helper'
+# frozen_string_literal: true
 
-RSpec.describe V1::Users::SessionsController, type: :controller do
+describe V1::Users::SessionsController, type: :controller do
+  describe 'POST #create' do
+    let(:user) { create :user }
 
+    it 'returns a http success if valid params are passed' do
+      post :create, params: { email: user.email, password: user.password }
+      expect(response).to have_http_status 200
+    end
+
+    it 'returns a valid jwt if valid params are passed' do
+      post :create, params: { email: user.email, password: user.password }
+      returned = JSON.parse(response.body)
+      expect(returned).to include 'jwt'
+    end
+  end
 end
