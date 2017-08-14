079764a123e28029d5d7a9aa1cfca60d435078d5
p150 <pavanprakash21@gmail.com>
2017-08-14 22:48:08 +0530

    Remove unused class methods from UserIdentity

--- app/models/user_identity.rb
+++ app/models/user_identity.rb
@@ -4,12 +4,4 @@ class UserIdentity < ApplicationRecord
   belongs_to :user
 
   validates :uid, :provider, presence: true, uniqueness: { case_sensitive: false }
-
-  def self.find_with_omniauth(auth)
-    find_by(uid: auth['uid'], provider: auth['provider'])
-  end
-
-  def self.create_with_omniauth(auth)
-    create(uid: auth['uid'], provider: auth['provider'])
-  end
 end
