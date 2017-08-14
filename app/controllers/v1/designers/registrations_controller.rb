66cc04395d25be7e9672790549b1d2b5c1911aed
p150 <pavanprakash21@gmail.com>
2017-08-15 01:05:27 +0530

    Add basic controller files

---
+++ app/controllers/v1/designers/registrations_controller.rb
@@ -0,0 +1,2 @@
+class V1::Designers::RegistrationsController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -22,4 +22,23 @@ Rails.application.routes.draw do
       end
     end
   end
+
+  constraints subdomain: 'api.designer' do
+    scope module: :v1, constraints: RouteConstraints.new(version: 1, default: true), defaults: { format: :json } do
+      scope module: :designers do
+        # Registration
+        post 'sign-up', to: 'registrations#create'
+        get 'confirm/:token', to: 'registrations#confirm'
+        post 'resend-confirmation-token', to: 'registrations#resend_confirmation'
+        post 'send-reset-password-instructions', to: 'registrations#send_reset_password_instructions'
+        get 'reset-password/:token', to: 'registrations#reset_password'
+        post 'update-password', to: 'registrations#update_password'
+        get 'resend-otp', to: 'registrations#resend_otp'
+        post 'verify-otp', to: 'registrations#verify_otp'
+
+        # Authentication
+        post 'login', to: 'sessions#create'
+      end
+    end
+  end
 end
