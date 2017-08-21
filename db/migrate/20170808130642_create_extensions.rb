919d57d3308fce9b89b35ed65444e392647acdcd
p150 <pavanprakash21@gmail.com>
2017-08-21 13:55:41 +0530

    Remove gist index extension from the migration

--- db/migrate/20170808130642_create_extensions.rb
+++ db/migrate/20170808130642_create_extensions.rb
@@ -1,7 +1,6 @@
 class CreateExtensions < ActiveRecord::Migration[5.1]
   def change
     enable_extension 'btree_gin' unless extension_enabled?('btree_gin')
-    enable_extension 'btree_gist' unless extension_enabled?('btree_gist')
     enable_extension 'pg_trgm' unless extension_enabled?('pg_trgm')
     enable_extension 'pgcrypto' unless extension_enabled?('pgcrypto')
   end
