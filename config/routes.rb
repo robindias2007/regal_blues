3693d9b40feeec04ac82816b40b5c8c3b3e88e55
p150 <pavanprakash21@gmail.com>
2017-09-02 12:51:15 +0530

    Add default url options for routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
+Rails.application.routes.default_url_options = {
+    host: 'custumise.com'
+}
+
 Rails.application.routes.draw do
   root 'home#index'
   # namespace 'api.user' do
