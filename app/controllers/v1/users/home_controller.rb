613a1c9755149d0d38cacc7fa331dadb5824884c
p150 <pavanprakash21@gmail.com>
2017-09-11 16:10:48 +0530

    Refactor home#mobile

--- app/controllers/v1/users/home_controller.rb
+++ app/controllers/v1/users/home_controller.rb
@@ -1,29 +1,49 @@
 # frozen_string_literal: true
 
 class V1::Users::HomeController < V1::Users::BaseController
-  skip_before_action :authenticate
+  skip_before_action :authenticate, only: :mobile
 
   def mobile
     if current_user.present?
-      if current_user.live_orders?
-        orders = current_user.orders.order(created_at: :desc).limit(3)
-        render json: { orders: order_resource(orders), requests: [], recos: [], top_designers: [] }
-      elsif current_user.requests_but_no_orders?
-        requests = current_user.requests.includes(:sub_category).order(created_at: :desc).limit(3)
-        render json: { requests: request_resource(requests), recos: [], top_designers: [], orders: [] }
-      elsif current_user.no_requests_or_orders?
-        # TODO: Design a recommendation engine
-        recos = Product.includes(designer: :designer_store_info).order('RANDOM()').limit(6)
-        render json: { recos: recommendation_resource(recos), top_designers: [], orders: [], requests: [] }
-      end
+      orders_or_requests_or_recommendations
     else
-      # TODO: Design an algorithm to quickly calculate the ratings of a designer
-      top_designers = Designer.includes(:designer_store_info, :sub_categories).order('RANDOM()').limit(6)
-      render json: { top_designers: td_resource(top_designers), recos: [], orders: [], requests: [] }
+      render_top_designers
     end
   end
 
   private
+
+  def orders_or_requests_or_recommendations
+    if current_user.live_orders?
+      render_orders
+    elsif current_user.requests_but_no_orders?
+      render_requests
+    elsif current_user.no_requests_or_orders?
+      render_recos
+    end
+  end
+
+  def render_orders
+    orders = current_user.orders.order(created_at: :desc).limit(3)
+    render json: { orders: order_resource(orders), requests: [], recos: [], top_designers: [] }
+  end
+
+  def render_requests
+    requests = current_user.requests.includes(:sub_category).order(created_at: :desc).limit(3)
+    render json: { requests: request_resource(requests), recos: [], top_designers: [], orders: [] }
+  end
+
+  def render_recos
+    # TODO: Design a recommendation engine
+    recos = Product.includes(designer: :designer_store_info).order('RANDOM()').limit(6)
+    render json: { recos: recommendation_resource(recos), top_designers: [], orders: [], requests: [] }
+  end
+
+  def render_top_designers
+    # TODO: Design an algorithm to quickly calculate the ratings of a designer
+    top_designers = Designer.includes(:designer_store_info, :sub_categories).order('RANDOM()').limit(6)
+    render json: { top_designers: td_resource(top_designers), recos: [], orders: [], requests: [] }
+  end
 
   def td_resource(top_designers)
     td_options = { each_serializer: V1::Users::TopDesignersSerializer }
