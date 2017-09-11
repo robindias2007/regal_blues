7e1d5abb3b0c27897659833d3161cd3d1e91b357
p150 <pavanprakash21@gmail.com>
2017-09-11 17:01:25 +0530

    Make all error messages from strings to array of strings

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::API
       yield if block_given?
       render json: { message: success[0] }, status: success[1]
     else
-      render json: { errors: failure[0] }, status: failure[1]
+      render json: { errors: [failure[0]] }, status: failure[1]
     end
   end
 end
