91f8807a5763e55b8d6f8b4466e582a91195e79f
p150 <pavanprakash21@gmail.com>
2017-09-27 14:19:25 +0530

    Change the way images are made

--- app/models/request_image.rb
+++ app/models/request_image.rb
@@ -1,19 +1,22 @@
 # frozen_string_literal: true
 
 class RequestImage < ApplicationRecord
-  belongs_to :request
+  belongs_to :request, optional: true
 
   validates :serial_number, presence: true
   validates :width, :height, :serial_number, numericality: { only_integer: true }
 
-  before_validation :make_image, if: :blank_image?
+  before_save :make_image, if: :blank_image?
 
   mount_base64_uploader :image, ImageUploader
 
   private
 
   def make_image
-    self.image = `convert -size 1024x1024 canvas:${self.color} INLINE:PNG:-`
+    require 'open3'
+    self.image = Open3.popen3("convert -size 1024x1024 canvas:#{color} INLINE:PNG:-") do |_, out, _, _|
+      out.read.chomp
+    end
   end
 
   def blank_image?
