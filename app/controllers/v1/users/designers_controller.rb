03ca414e0e94f1a798305140000a7d5061c75801
p150 <pavanprakash21@gmail.com>
2017-10-09 18:24:45 +0530

    Modify designers controller for users

--- app/controllers/v1/users/designers_controller.rb
+++ app/controllers/v1/users/designers_controller.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
 class V1::Users::DesignersController < V1::Users::BaseController
+  skip_before_action :authenticate
+
   def index
     designers = Designer.all.order(full_name: :asc)
-    render json: { designers: designers }
+    render json: designers, each_serializer: V1::Users::TopDesignersSerializer
   end
 end
