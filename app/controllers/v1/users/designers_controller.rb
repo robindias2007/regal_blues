9501cc5a26ca284fa5fe40e5f62f975d62d748a4
p150 <pavanprakash21@gmail.com>
2017-10-09 18:43:13 +0530

    Optimise designers render for users

--- app/controllers/v1/users/designers_controller.rb
+++ app/controllers/v1/users/designers_controller.rb
@@ -4,7 +4,8 @@ class V1::Users::DesignersController < V1::Users::BaseController
   skip_before_action :authenticate
 
   def index
-    designers = Designer.all.order(full_name: :asc)
+    designers = Designer.includes(:sub_categories, :designer_categorizations, :designer_store_info)
+                        .all.order(full_name: :asc)
     render json: designers, each_serializer: V1::Users::TopDesignersSerializer
   end
 end
