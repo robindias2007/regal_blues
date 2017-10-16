1d18a79734956ffb3548c482569cdebbbcc716eb
p150 <pavanprakash21@gmail.com>
2017-10-16 18:29:16 +0530

    Add safe toggle to request designer

--- app/models/request_designer.rb
+++ app/models/request_designer.rb
@@ -9,4 +9,8 @@ class RequestDesigner < ApplicationRecord
   def self.find_for(request_id, designer)
     find_by(request_id: request_id, designer: designer)
   end
+
+  def safe_toggle!(attr)
+    public_send(attr) == true ? update(:"#{attr}" => false) : update(:"#{attr}" => true)
+  end
 end
