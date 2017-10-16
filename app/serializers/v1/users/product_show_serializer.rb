f3af0bae8e3ebed5ec228bbf58c96cba7d0b8775
p150 <pavanprakash21@gmail.com>
2017-10-16 18:04:24 +0530

    Change the product serializers for user

--- app/serializers/v1/users/product_show_serializer.rb
+++ app/serializers/v1/users/product_show_serializer.rb
@@ -1,15 +1,32 @@
 # frozen_string_literal: true
 
 class V1::Users::ProductShowSerializer < ActiveModel::Serializer
-  attributes :id, :name, :designer_name, :selling_price, :image, :more_items, :similar_items
+  attributes :id, :name, :designer_name, :selling_price, :designer_avatar, :more_items, :similar_items, :description,
+    :additional_info, :images
 
   def designer_name
     object&.designer&.designer_store_info&.display_name || 'Generic Store Name'
   end
 
-  def image
+  def designer_avatar
     # TODO: Change this to the cover image of the store
     object.designer.avatar || 'Default Image URL'
+  end
+
+  def images
+    object.images.map do |image|
+      ActiveModelSerializers::SerializableResource.new(image,
+        serializer: ImageSerializer).as_json
+    end
+  end
+
+  def additional_info
+    {
+      fabric: object.product_info.fabric,
+      care:   object.product_info.care,
+      notes:  object.product_info.notes,
+      work:   object.product_info.work
+    }
   end
 
   def more_items
--- app/serializers/v1/users/products_serializer.rb
+++ app/serializers/v1/users/products_serializer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class V1::Users::ProductsSerializer < ActiveModel::Serializer
-  attributes :id, :name, :designer_name, :selling_price, :image
+  attributes :id, :name, :designer_name, :selling_price, :image, :sub_category
 
   def designer_name
     object&.designer&.designer_store_info&.display_name || 'Generic Store Name'
@@ -11,5 +11,9 @@ class V1::Users::ProductsSerializer < ActiveModel::Serializer
     # TODO: Change this to the cover image of the store
     # object.designer.avatar || 'Default Image URL'
     object.images.first.image.url || 'Default Image URL'
+  end
+
+  def sub_category
+    object.sub_category.name
   end
 end
