00232a7856d3b68b1310792b05a503a3884f3b5d
p150 <pavanprakash21@gmail.com>
2017-10-16 18:21:49 +0530

    Add designer filter and sorting for users

--- app/controllers/v1/users/designers_controller.rb
+++ app/controllers/v1/users/designers_controller.rb
@@ -4,13 +4,24 @@ class V1::Users::DesignersController < V1::Users::BaseController
   skip_before_action :authenticate
 
   def index
-    designers = Designer.includes(:sub_categories, :designer_categorizations, :designer_store_info)
-                        .all.order(full_name: :asc)
+    designers = conditional_render(params[:min_order], params[:max_order])
     render json: designers, each_serializer: V1::Users::TopDesignersSerializer
   end
 
   def show
     designer = Designer.includes(:designer_store_info).find(params[:id])
     render json: designer, serializer: V1::Users::DesignerShowSerializer
+  end
+
+  private
+
+  def conditional_render(min_order, max_order)
+    if min_order.present?
+      Designer.min_order_greater_than(min_order)
+    elsif max_order.present?
+      Designer.max_order_greater_than(max_order)
+    else
+      Designer.included_releated_associations.all
+    end.order_alphabetically
   end
 end
--- app/models/designer.rb
+++ app/models/designer.rb
@@ -14,12 +14,28 @@ class Designer < ApplicationRecord
   validates :full_name, :email, :mobile_number, :location, presence: true
   validates :email, :mobile_number, uniqueness: { case_sensitive: false }
 
+  scope :order_alphabetically, -> { order(full_name: :asc) }
+
   before_create :generate_pin
 
   mount_base64_uploader :avatar, AvatarUploader
 
   def self.find_for_category(category_id)
     joins(:designer_categorizations).where(designer_categorizations: { sub_category_id: category_id })
+  end
+
+  def self.included_releated_associations
+    includes(:sub_categories, :designer_categorizations, :designer_store_info)
+  end
+
+  def self.min_order_greater_than(min_value)
+    included_releated_associations.joins(:designer_store_info)
+                                  .where(designer_store_infos: { min_order_price: min_value.to_i..Float::INFINITY })
+  end
+
+  def self.max_order_greater_than(max_value)
+    included_releated_associations.joins(:designer_store_info)
+                                  .where(designer_store_infos: { min_order_price: 0..max_value.to_i })
   end
 
   def notify_request(request)
