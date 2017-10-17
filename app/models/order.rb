ed68e3c23628fee2eab483a844bf40c90005618e
p150 <pavanprakash21@gmail.com>
2017-10-17 13:54:35 +0530

    Add aasm block to order model

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,73 @@
 # frozen_string_literal: true
 
+# User can either select the options before paying or ask for more options after paying.
+# If the options are preselcted, state will transition from new -> paid -> awaiting_confirmation
+# If the user asks for more options, state will transition from new -> paid -> awaiting_options.
+# Then, awaiting_confirmation will be given.
+
 class Order < ApplicationRecord
+  include AASM
+
   belongs_to :designer
   belongs_to :user
   belongs_to :offer_quotation
+
+  aasm column: 'status' do
+    state :started, initial: true
+    state :paid
+    state :awaiting_options
+    state :awaiting_confirmation
+    state :awaiting_measurements
+    state :confirmed
+    state :shipped_to_qc
+    state :in_qc
+    state :shipped_to_user
+    state :delivered
+    state :rejected
+
+    # after_all_transitions :broadcast_notification
+
+    event :pay do
+      transitions from: :started, to: :paid
+    end
+
+    event :await_options do
+      transitions from: :paid, to: :awaiting_options
+    end
+
+    # When a user completes selecting the options
+    event :await_confirmation do
+      transitions from: :awaiting_options, to: :awaiting_confirmation
+    end
+
+    # When a designer confirms the order.
+    event :confirmation do
+      transitions from: :awaiting_confirmation, to: :confirmed
+    end
+
+    # When a designer clicks on the ship_to_qc button. If he/she forgets, support should be able to do it.
+    event :ship_to_qc do
+      transitions from: :confirmed, to: :shipped_to_qc
+    end
+
+    # Support clicks the recieved product button for this order
+    event :quality_control do
+      transitions from: :shipped_to_qc, to: :in_qc
+    end
+
+    # Support ships the product / Delivery guy picks up the order to be shipped
+    event :ship do
+      transitions from: :in_qc, to: :shipped_to_user
+    end
+
+    # When the delivery partner notifies us of a product being delivered
+    event :deliver do
+      transitions from: :shipped_to_user, to: :delivered
+    end
+
+    # When the support rejects the product due to low quality/other reasons
+    event :reject do
+      transitions from: :in_qc, to: :rejected
+    end
+  end
 end
