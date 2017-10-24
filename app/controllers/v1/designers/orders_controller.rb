3cf896690201d04a92aad1f406bfc7b3c1fd30e6
p150 <pavanprakash21@gmail.com>
2017-10-24 13:17:56 +0530

    add todos

--- app/controllers/v1/designers/orders_controller.rb
+++ app/controllers/v1/designers/orders_controller.rb
@@ -16,6 +16,7 @@ class V1::Designers::OrdersController < V1::Designers::BaseController
     order = current_designer.orders.find(params[:id])
     if order.paid? && order.all_options_selected?
       order.designer_confirms!
+      # TODO: Notify the user
       render json: { message: 'Order has been marked as confirmed. User will be notified of the same.' }
     else
       render json: { errors:  order.errors,
