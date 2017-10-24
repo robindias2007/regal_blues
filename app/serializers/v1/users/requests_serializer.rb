1cf9a2a6453857b59bc17188f9f39dba6f2f5611
p150 <pavanprakash21@gmail.com>
2017-10-24 17:20:54 +0530

    Modify a bit

--- app/serializers/v1/users/requests_serializer.rb
+++ app/serializers/v1/users/requests_serializer.rb
@@ -1,8 +1,7 @@
 # frozen_string_literal: true
 
 class V1::Users::RequestsSerializer < ActiveModel::Serializer
-  # attributes :id, :name, :item_type, :min_budget, :max_budget, :timeline, :offers_count
-  attributes :id, :name, :item_type, :max_budget, :timeline, :offers_count, :status
+  attributes :id, :name, :item_type, :min_budget, :max_budget, :timeline, :offers_count, :status
 
   def item_type
     SubCategory.find(object.sub_category_id).name
