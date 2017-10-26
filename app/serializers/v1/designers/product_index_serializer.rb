786242790599b4b7ce9f4d2c16c64e2d08385d2b
p150 <pavanprakash21@gmail.com>
2017-10-26 12:04:30 +0530

    Order images in product index for designers

--- app/serializers/v1/designers/product_index_serializer.rb
+++ app/serializers/v1/designers/product_index_serializer.rb
@@ -4,7 +4,7 @@ class V1::Designers::ProductIndexSerializer < ActiveModel::Serializer
   attributes :id, :name, :selling_price, :active, :sku, :image, :category
 
   def image
-    object.images.first&.image || 'Default Image URL'
+    object.images.order(created_at: :asc).first&.image || 'Default Image URL'
   end
 
   def category
