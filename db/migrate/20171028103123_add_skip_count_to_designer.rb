620bfbe2fe34d197bb02c1432da93fc42a882265
p150 <pavanprakash21@gmail.com>
2017-10-28 16:02:08 +0530

    Add skip count to designers

---
+++ db/migrate/20171028103123_add_skip_count_to_designer.rb
@@ -0,0 +1,5 @@
+class AddSkipCountToDesigner < ActiveRecord::Migration[5.1]
+  def change
+    add_column :designers, :skip_count, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171028102147) do
+ActiveRecord::Schema.define(version: 20171028103123) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -133,6 +133,7 @@ ActiveRecord::Schema.define(version: 20171028102147) do
     t.boolean "active"
     t.text "bio"
     t.string "live_status"
+    t.integer "skip_count"
     t.index ["active"], name: "index_designers_on_active", where: "active"
     t.index ["confirmation_token"], name: "index_designers_on_confirmation_token", unique: true
     t.index ["email"], name: "index_designers_on_email", unique: true
