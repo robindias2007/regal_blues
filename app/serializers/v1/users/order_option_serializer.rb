e24ba7033405572d381444a115dcf2dfecfac908
p150 <pavanprakash21@gmail.com>
2017-10-30 19:03:01 +0530

    Finish order detail serializer for users

---
+++ app/serializers/v1/users/order_option_serializer.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+class V1::Users::OrderOptionSerializer < ActiveModel::Serializer
+  attributes :id, :gallery_name, :image, :more_options, :designer_pick
+
+  def gallery_name
+    object.offer_quotation_gallery.name
+  end
+
+  def image
+    ActiveModelSerializers::SerializableResource.new(object&.image,
+      serializer: ImageSerializer).as_json
+  end
+
+  def more_options
+    object&.more_options
+  end
+
+  def designer_pick
+    object&.designer_pick
+  end
+end
--- app/serializers/v1/users/order_show_serializer.rb
+++ app/serializers/v1/users/order_show_serializer.rb
@@ -1,5 +1,45 @@
 # frozen_string_literal: true
 
 class V1::Users::OrderShowSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :request_id, :request_image, :designer_name, :category, :timeline, :paid_price, :designer_note,
+    :added_notes, :selections
+
+  def request_id
+    object.offer_quotation.offer.request.id
+  end
+
+  def request_image
+    object.offer_quotation.offer.request.request_images.order(created_at: :asc).first.image
+  end
+
+  def designer_name
+    object.offer_quotation.offer.designer.designer_store_info.display_name
+  end
+
+  def category
+    object.offer_quotation.offer.request.sub_category.name
+  end
+
+  def timeline
+    object.offer_quotation.offer.request.timeline
+  end
+
+  def paid_price
+    object.offer_quotation.price
+  end
+
+  def designer_note
+    object.offer_quotation.description
+  end
+
+  def added_notes
+    object.offer_quotation.designer_note
+  end
+
+  def selections
+    object.order_options.map do |option|
+      ActiveModelSerializers::SerializableResource.new(option,
+        serializer: V1::Users::OrderOptionSerializer).as_json
+    end
+  end
 end
