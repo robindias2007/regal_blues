3bbf8bcd7ca1812e7cfe138c928980d9325cdeb8
p150 <pavanprakash21@gmail.com>
2017-11-02 10:14:48 +0530

    Change routes to post

--- config/routes.rb
+++ config/routes.rb
@@ -140,9 +140,9 @@ Rails.application.routes.draw do
         member do
           patch :confirm
           get :fabric_unavailable_data, path: 'fabric-unavailable'
-          put :fabric_unavailable, path: 'fabric-unavailable'
+          post :fabric_unavailable, path: 'fabric-unavailable'
           get :give_more_options_data, path: 'give-more-options'
-          put :give_more_options_data, path: 'give-more-options'
+          post :give_more_options_data, path: 'give-more-options'
           patch :toggle_active_gallery_image, path: 'toggle-active-image'
         end
       end
