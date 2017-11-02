b6ea287e04719428eb431cb6379f3b419408d3ef
p150 <pavanprakash21@gmail.com>
2017-11-02 10:19:09 +0530

    Add new tag to image

--- app/serializers/image_serializer.rb
+++ app/serializers/image_serializer.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 class ImageSerializer < ActiveModel::Serializer
-  attributes :id, :image, :width, :height, :description, :serial_number, :disabled
+  attributes :id, :image, :width, :height, :description, :serial_number, :disabled, :new
 end
