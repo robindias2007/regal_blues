cb922740a1ff26d558592b0912b597e14594dfaf
p150 <pavanprakash21@gmail.com>
2017-11-06 14:23:27 +0530

    Add connection funcitonality

--- app/channels/support_chat_channel.rb
+++ app/channels/support_chat_channel.rb
@@ -15,25 +15,25 @@ class SupportChatChannel < ApplicationCable::Channel
   end
 
   # This receives the data even the method name tells something else.
-  def send_convo(chat_params)
-    support_chat = SupportChat.find_by(designer: current_user)
-    convo = support_chat.conversations.build(chat_params)
-    if convo.save
-      send_convo(convo)
-    else
-      broadcast_error_to_sender(convo)
-    end
+  def send_convo(_chat_params)
+    connect_to_support
+    # support_chat = SupportChat.find_by(designer: current_user)
+    # convo = support_chat.conversations.build(chat_params)
+    # if convo.save
+    #   send_convo(convo)
+    # else
+    #   broadcast_error_to_sender(convo)
+    # end
   end
 
   def connect_to_support
-    support_chat = SupportChat.find_or_initialize_by(designer: current_designer)
-    binding.pry
+    support_chat = SupportChat.find_or_initialize_by(designer: current_user)
     if support_chat.present?
-      broadcast_connection_established
+      broadcast_connection_new
       logger.add_tags 'SupportChatChannel', 'support_chat already present'
     else
       support_chat.save
-      broadcast_connection_failure
+      broadcast_connection_existing
       logger.add_tags 'SupportChatChannel', 'support_chat created'
     end
   end
@@ -50,12 +50,12 @@ class SupportChatChannel < ApplicationCable::Channel
       error: convo.errors)
   end
 
-  def broadcast_connection_established
+  def broadcast_connection_new
     ActionCable.server.broadcast("support_chats_#{current_user.class.name.downcase}_#{current_user.id}",
       message: 'Connection established with the support.')
   end
 
-  def broadcast_connection_failure
+  def broadcast_connection_existing
     ActionCable.server.broadcast("support_chats_#{current_user.class.name.downcase}_#{current_user.id}",
       message: 'New connection has been established')
   end
