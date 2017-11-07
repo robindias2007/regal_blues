d4ea9d30853c86c0a5c9ab2fa00651902362c9f4
p150 <pavanprakash21@gmail.com>
2017-11-07 12:01:34 +0530

    Add validations to support

--- app/models/support.rb
+++ app/models/support.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class Support < ApplicationRecord
+  validate :full_name, :mobile_number, presence: true
+  validates :email, format:     { with: /\A[A-Z0-9._%a-z\-]+@+amidostech\.com\z/, message: 'not valid email address' },
+                    uniqueness: { case_sensitive: false }
+
   has_many :support_chats, dependent: :destroy
   has_many :sent_conversations, as: :sender, dependent: :destroy, class_name: 'Conversation'
   has_many :received_conversations, as: :receiver, dependent: :destroy, class_name: 'Conversation'
