07b1e966d0195c39bee7e2aabe0788394c32da75
p150 <pavanprakash21@gmail.com>
2017-11-13 12:22:06 +0530

    Add allowed origin to action cable in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,7 +38,9 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   config.action_cable.url = 'ws://custumiseapi-env.gpmw6833qc.ap-south-1.elasticbeanstalk.com/cable'
   config.action_cable.allowed_request_origins = ['http://custumiseapi-env.gpmw6833qc.ap-south-1.elasticbeanstalk.com',
-                                                 'https://custumiseapi-env.gpmw6833qc.ap-south-1.elasticbeanstalk.com']
+                                                 'https://custumiseapi-env.gpmw6833qc.ap-south-1.elasticbeanstalk.com',
+                                                 'ws://custumiseapi-env.gpmw6833qc.ap-south-1.elasticbeanstalk.com',
+                                                 'wss://custumiseapi-env.gpmw6833qc.ap-south-1.elasticbeanstalk.com']
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
