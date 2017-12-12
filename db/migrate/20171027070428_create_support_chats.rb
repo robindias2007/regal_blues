c13840ca65c49f83804b9b545f74bdc7acb776c7
Robin Dias <robin.dias@amidostech.com>
2017-12-12 15:36:10 +0530

    support chat errors so commented out for now

--- db/migrate/20171027070428_create_support_chats.rb
+++ db/migrate/20171027070428_create_support_chats.rb
@@ -1,12 +1,12 @@
 class CreateSupportChats < ActiveRecord::Migration[5.1]
   def change
-    create_table :support_chats, id: :uuid do |t|
-      t.references :support, type: :uuid, index: true, foreign_key: true
-      t.references :user, type: :uuid, index: true, foreign_key: true
-      t.references :designer, type: :uuid, index: true, foreign_key: true
-      t.boolean :responding
+    # create_table :support_chats, id: :uuid do |t|
+    #   t.references :support, type: :uuid, index: true, foreign_key: true
+    #   t.references :user, type: :uuid, index: true, foreign_key: true
+    #   t.references :designer, type: :uuid, index: true, foreign_key: true
+    #   t.boolean :responding
 
-      t.timestamps
+    #   t.timestamps
     end
   end
 end
