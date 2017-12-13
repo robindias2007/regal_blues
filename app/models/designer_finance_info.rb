0ec49a7e69b5675ebe28425143b20a9bab7a1503
Robin Dias <robin.dias@amidostech.com>
2017-12-13 14:02:57 +0530

    flow of the site and otp correction

--- app/models/designer_finance_info.rb
+++ app/models/designer_finance_info.rb
@@ -3,9 +3,11 @@
 class DesignerFinanceInfo < ApplicationRecord
   belongs_to :designer
 
-  validates :bank_name, :bank_branch, :ifsc_code, :account_number, :blank_cheque_proof, :personal_pan_number,
-    :personal_pan_number_proof, :business_pan_number, :business_pan_number_proof, :tin_number, :tin_number_proof,
-    :gstin_number, :gstin_number_proof, :business_address_proof, presence: true
+  validates :bank_name, :bank_branch, :ifsc_code, :account_number, :blank_cheque_proof ,
+    :personal_pan_number_proof, :business_pan_number, :business_pan_number_proof, :tin_number_proof,
+    :gstin_number_proof, :business_address_proof, presence: true
+
+  # validates :tin_number, :gstin_number, :personal_pan_number, presence: true
 
   validates :account_number, :business_pan_number, :personal_pan_number, :tin_number, :gstin_number,
     uniqueness: { case_sensitive: false }
