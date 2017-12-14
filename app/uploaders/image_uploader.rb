7eef7301aabf6ea53cb3f78a75c05f996a2a2ca2
Robin Dias <robin.dias@amidostech.com>
2017-12-14 19:36:11 +0530

    image

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -22,7 +22,7 @@ class ImageUploader < CarrierWave::Uploader::Base
     else
       store_url = "http://192.168.0.103:3090/"
     end
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "#{store_url}uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
