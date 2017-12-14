486b36c75834936fe9b0b1d004982d3458a14388
Robin Dias <robin.dias@amidostech.com>
2017-12-14 13:15:28 +0530

    debugger removed

--- app/controllers/v1/users/requests_controller.rb
+++ app/controllers/v1/users/requests_controller.rb
@@ -5,7 +5,6 @@ class V1::Users::RequestsController < V1::Users::BaseController
     request = current_user.requests.build(request_params)
     
     return no_designers_selected if params[:request][:request_designers_attributes].nil?
-    debugger
     if request.save! && request.request_designers.create!(request_designers_params['request_designers_attributes'])
       # RequestDesignerService.notify_about request
       render json: { message: 'Request saved successfully' }, status: 201
