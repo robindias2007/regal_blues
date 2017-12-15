49b52c7ad023f48cb035472fa4119f7f1ab7b286
Robin Dias <robin.dias@amidostech.com>
2017-12-15 15:50:47 +0530

    changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,7 +33,6 @@ Rails.application.configure do
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
-  config.root_url = 'https://limitless-brook-27912.herokuapp.com'
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
