eda210e1f68f0c5848463dbba9745e5ccfdc1e1d
Hitesh kanwar <hiteshgurjar786@gmail.com>
2017-12-26 15:53:13 +0530

    conversation fix

--- app/controllers/v1/users/conversations_controller.rb
+++ app/controllers/v1/users/conversations_controller.rb
@@ -12,7 +12,7 @@ class V1::Users::ConversationsController < V1::Users::BaseController
   end
 
   def create
-    current_user.conversations.new(conversation_params)
+    conversation = current_user.conversations.new(conversation_params)
     if conversation.save
       render json: {conversation: conversation}, status: 201
     else
