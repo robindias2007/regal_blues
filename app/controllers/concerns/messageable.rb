5395a7005c53ea719c3c971076a47034cca328f5
Hitesh kanwar <hiteshgurjar786@gmail.com>
2017-12-28 18:40:39 +0530

    message response

--- app/controllers/concerns/messageable.rb
+++ app/controllers/concerns/messageable.rb
@@ -15,7 +15,8 @@ module Messageable
 		conversation = Conversation.find(params[:id])
 		message = conversation.messages.new(message_params)
 		if message.save!
-			render json: {message: Message.as_a_json(message)}, status: 201
+			# render json: {message: Message.as_a_json(message)}, status: 201
+			render json: {message: message}, status: 201
 		else
 			render json: {message: message.errors}, status: 400
 		end
