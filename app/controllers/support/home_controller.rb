c4d335420b354ce9b2e90fa09c6f37fa1f6fed1c
Robin Dias <robin.dias@amidostech.com>
2017-12-28 16:22:10 +0530

    support logout go to sign up screen

--- app/controllers/support/home_controller.rb
+++ app/controllers/support/home_controller.rb
@@ -2,13 +2,17 @@
 
 class Support::HomeController < ApplicationController
   def index
-    @data = {
-      total_users:     User.count,
-      total_designers: Designer.count,
-      total_requests:  Request.count,
-      total_offers:    OfferQuotation.count,
-      total_orders:    Order.count,
-      total_products:  Product.count
-    }
+    if support_signed_in?
+      @data = {
+        total_users:     User.count,
+        total_designers: Designer.count,
+        total_requests:  Request.count,
+        total_offers:    OfferQuotation.count,
+        total_orders:    Order.count,
+        total_products:  Product.count
+      }
+    else
+      redirect_to '/supports/sign_in'
+    end
   end
 end
