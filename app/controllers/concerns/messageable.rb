25c88d720e13704184359fe95e0e7850807c433c
Hitesh kanwar <hiteshgurjar786@gmail.com>
2017-12-28 17:04:31 +0530

    added sender_id and receiver_id to message

--- app/controllers/concerns/messageable.rb
+++ app/controllers/concerns/messageable.rb
@@ -5,7 +5,7 @@ module Messageable
 		conversation = Conversation.find(params[:id])
 		messages = conversation.messages if conversation.present?
 		if messages.present?
-			render json: {messages: messages}, status: 201
+			render json: {messages: Message.as_json(messages)}, status: 201
 		else
 			render json: {messages: "No message avilable"}, status: 400
 		end
@@ -15,7 +15,7 @@ module Messageable
 		conversation = Conversation.find(params[:id])
 		message = conversation.messages.new(message_params)
 		if message.save!
-			render json: {message: message}, status: 201
+			render json: {message: Message.as_a_json(message)}, status: 201
 		else
 			render json: {message: message.errors}, status: 400
 		end
