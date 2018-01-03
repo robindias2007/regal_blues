c3744bb5ec385389a61f8848fd23ee27d2c4f1a1
Hitesh kanwar <hiteshgurjar786@gmail.com>
2018-01-03 19:05:32 +0530

    Fixed color to image issue

--- app/models/request_image.rb
+++ app/models/request_image.rb
@@ -6,21 +6,21 @@ class RequestImage < ApplicationRecord
   validates :serial_number, presence: true
   validates :width, :height, :serial_number, numericality: { only_integer: true }
 
-  #before_save :make_image, if: :blank_image?
+  before_save :make_image, if: :blank_image?
 
   mount_base64_uploader :image, ImageUploader
 
-  private
+    private
 
-  def make_image
-    require 'open3'
-    image_data = Open3.popen3("convert -size 1024x1024 canvas:#{color} PNG:-| base64") do |_, out, _, _|
-      out.read.chomp
+    def make_image
+      require 'open3'
+      image_data = Open3.popen3("convert -size 1024x1024 canvas:#{color} PNG:-| base64") do |_, out, _, _|
+        out.read.chomp
+      end
+      self.image = 'data:image/png;base64,' + image_data
     end
-    self.image = 'data:image/png;base64,' + image_data
-  end
 
-  def blank_image?
-    image.blank?
-  end
+    def blank_image?
+      image.blank?
+    end
 end
