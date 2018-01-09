7612996fd829238600cd64b7d9ac8ab22872df9c
Hitesh kanwar <hiteshgurjar786@gmail.com>
2018-01-09 13:48:34 +0530

    Change the URL in cors.rb file

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -11,7 +11,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'http://localhost:3000', 'custumisedesigner.herokuapp.com', 'designer.custumise.com'
 
     resource '*',
       headers: :any,
