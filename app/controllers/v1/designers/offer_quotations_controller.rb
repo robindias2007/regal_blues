9ea31219c81e0deed55da717128189dfcd1d760a
Robin Dias <robin.dias@amidostech.com>
2018-01-15 18:11:18 +0530

    update measurement tags:predefined list

--- app/controllers/v1/designers/offer_quotations_controller.rb
+++ app/controllers/v1/designers/offer_quotations_controller.rb
@@ -10,4 +10,18 @@ class V1::Designers::OfferQuotationsController < V1::Designers::BaseController
     quotation = current_designer.offers.find(params[:offer_id]).offer_quotations.find(params[:id])
     render json: quotation
   end
+
+
+  #Predefined Measurement Tags To Be Shown while quoting it.
+  def measurement_tags
+    measurement_tags =  %W[Bust Waist Hip #{"Cap Sleeve Length"} #{"Shoulder to Apex"}
+    					#{"Short Sleeve Length"} #{"3/4 Sleeve Length"} #{"Full Sleeve Length"} 
+    					#{"Front Neck Depth"} #{"Cross Under Bust"} #{"Lower Waist"} Wrist
+    					#{"Thigh Round"} #{"Lower Thigh"} #{"Knee Round"} Calf #{"Ankle Round"} 
+    					#{"Waist Length (waist to ankle)"} #{"Full Length Shoulder to Ankle"} 
+    					#{"Bottom Length"} #{"Elbow Round"} Bicep #{"Back Neck Depth"} #{"Shoulder Neck Round"} 
+    					#{"Arm Hole"} ]
+    render json: { data: measurement_tags }
+  end
+
 end
