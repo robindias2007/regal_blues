7510c0683ca3129dda62fe861908b930b5d4444e
Hitesh <hiteshgurjar786@gmail.com>
2018-01-18 16:05:01 +0530

    push notification fix for more option for user

--- app/models/order.rb
+++ app/models/order.rb
@@ -95,7 +95,7 @@ class Order < ApplicationRecord
     state :rejected_by_qc, after_enter: :update_datetime, after_commit: :qc_reject_mail
     # User Path: More Options + User Agrees
     state :user_awaiting_more_options, after_enter: :update_datetime
-    state :designer_gave_more_options, after_enter: :update_datetime, after_commit: :new_option
+    state :designer_gave_more_options, after_enter: :update_datetime#, after_commit: :new_option
     state :user_selected_options, after_enter: :update_datetime
     # User Path: More Options + User Cancels / Fabric Unavailable + User Cancels
     state :user_cancelled, after_enter: :update_datetime
@@ -286,15 +286,15 @@ class Order < ApplicationRecord
   end
 
 
-  def new_option
-    begin
-      NotificationsMailer.new_option(self).deliver_later
-      body = "You have new options for Order #{ self.order_id } by #{ self.user.full_name }. Please select new options."
-      self.user.notifications.create(body: body, notificationable_type: "Order", notificationable_id: self.order.id)
-      Order.new.send_notification(self.user.devise_token, body, body)
-    rescue
-    end
-  end
+  # def new_option
+  #   begin
+  #     NotificationsMailer.new_option(self).deliver_later
+  #     body = "You have new options for Order #{ self.order_id } by #{ self.user.full_name }. Please select new options."
+  #     self.user.notifications.create(body: body, notificationable_type: "Order", notificationable_id: self.order.id)
+  #     Order.new.send_notification(self.user.devise_token, body, body)
+  #   rescue
+  #   end
+  # end
 
   private
 
--- app/models/order_option.rb
+++ app/models/order_option.rb
@@ -8,23 +8,9 @@ class OrderOption < ApplicationRecord
 
   validate :not_more_than_one_option
 
-  # after_create :more_options_show
-
   def not_more_than_one_option
     errors.add(:order_id, "Can't select more than one option") if (image_id.present? && more_options.present?) ||
                                                        (image_id.present? && designer_pick.present?) ||
                                                        (more_options.present? && designer_pick.present?)
-  end
-
-  def more_options_show
-  	if self.more_options
-  		NotificationsMailer.new_option(self.order).deliver_later
-      begin
-        body = "You have new options for Order #{ self.order.order_id } by #{ self.order.user.full_name }. Please select new options."
-        self.order.user.notifications.create(body: body, notificationable_type: "Order", notificationable_id: self.order.id)
-        OrderOption.new.send_notification(self.order.user.devise_token, body, body)
-      rescue
-      end
-  	end
   end
 end
