eaf56abd9f24b1b96f496ca497fe65ab1c21a9b9
Hitesh <hiteshgurjar786@gmail.com>
2018-01-22 15:57:46 +0530

    aws access key and id

--- app/services/sms_service.rb
+++ app/services/sms_service.rb
@@ -23,8 +23,8 @@ class SmsService
 
   def self.sns
     client = Aws::SNS::Client.new(region:            'us-east-1', # Don't change the region
-                                  access_key_id:     Rails.application.secrets[:aws_access_key_id],
-                                  secret_access_key: Rails.application.secrets[:aws_secret_access_key])
+                                  access_key_id:     ENV['AWS_ACCESS_KEY'],
+                                  secret_access_key: ENV['AWS_SECRET_KEY'])
     client.set_sms_attributes(attributes: { 'DefaultSMSType' => 'Transactional', 'DefaultSenderID' => 'AM1DOS' })
     client
   end
