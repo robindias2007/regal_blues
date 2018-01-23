2df04250f691d8f6a15fb98f592a057a1b47682e
Robin Dias <robin.dias@amidostech.com>
2018-01-23 17:01:36 +0530

    changes

--- db/schema.rb
+++ db/schema.rb
@@ -522,7 +522,7 @@ ActiveRecord::Schema.define(version: 20180118115303) do
     t.text "devise_token"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["mobile_number"], name: "index_users_on_mobile_number"
+    t.index ["mobile_number"], name: "index_users_on_mobile_number", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
   end
