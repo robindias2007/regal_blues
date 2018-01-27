339d25a7dde67c1239a02e8b3a55d28e53d8e357
Robin Dias <robin.dias@amidostech.com>
2018-01-27 13:32:58 +0530

    text changes

--- lib/push_notification.rb
+++ lib/push_notification.rb
@@ -29,7 +29,7 @@ module PushNotification
 	    notification = Houston::Notification.new(device: token)
 	    notification.badge = 1
 	    notification.sound = "default"
-	    notification.alert = {title: title(msg), body: "You have new chat message"}
+	    notification.alert = {title: title(msg), body: "You have a new chat message"}
 	    key = msg.conversation.receiver_type.singularize+"_id"
 	    data = msg.conversation.receiver_id
 	    extraData = {"#{key}": data, message: msg.body, type: "chat"}
