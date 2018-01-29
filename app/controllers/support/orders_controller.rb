0c62f24ce61075149dc5445abd6ce47bd5ec7e80
Robin Dias <robin.dias@amidostech.com>
2018-01-29 17:09:44 +0530

    order page update

--- app/controllers/support/orders_controller.rb
+++ app/controllers/support/orders_controller.rb
@@ -5,26 +5,26 @@ class Support::OrdersController < ApplicationController
   end
 
   def index
-  	@orders = Order.all.order(updated_at: :desc)
+  	@orders = Order.order(created_at: :desc).all
     @order = Order.find(params[:format]) rescue nil
       if params[:commit] == 'Ship_To_QC'         # it checks if the user has clicked publish the it updates the form with publish
         @order.update(status:"shipped_to_qc")
-        redirect_to support_orders_path
+        redirect_to request.referer
       elsif params[:commit] == 'Delivered_To_QC'
         @order.update(status:"delivered_to_qc")
-        redirect_to support_orders_path
+        redirect_to request.referer
       elsif params[:commit] == "In_QC"
         @order.update(status:"in_qc")
-        redirect_to support_orders_path
+        redirect_to request.referer
       elsif params[:commit] == 'Ship_To_User'
         @order.update(status:"shipped_to_user")
-        redirect_to support_orders_path
+        redirect_to request.referer
       elsif params[:commit] == 'Deliver_To_User'
         @order.update(status:"delivered_to_user")
-        redirect_to support_orders_path
+        redirect_to request.referer
       elsif params[:commit] == 'Reject'
         @order.update(status:"rejected_by_qc")
-        redirect_to support_orders_path
+        redirect_to request.referer
       end
   end
 
