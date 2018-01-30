a83b825420161b6e54455ea0478688e3778901ef
Robin Dias <robin.dias@amidostech.com>
2018-01-30 17:39:49 +0530

    change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -104,8 +104,8 @@ Rails.application.configure do
     address:              'smtp.gmail.com',
     port:                 587,
     domain:               'gmail.com',
-    user_name:            'noreply@custumise.com',
-    password:             'Welcome123!',
+    user_name:            'noreplyhere@custumise.com',
+    password:             'Mumbai78!',
     authentication:       :login,
     enable_starttls_auto: true
   }
