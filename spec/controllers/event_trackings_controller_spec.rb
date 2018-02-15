8e0f9e8fcaf31a2eaa5c8975d5a6a1379b397b52
Robin Dias <robin.dias@amidostech.com>
2018-02-15 16:18:00 +0530

    remove all old files

---
+++ spec/controllers/event_trackings_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe EventTrackingsController, type: :controller do
+
+end
---
+++ spec/factories/event_trackings.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :event_tracking do
+    resource_type "MyString"
+    username "MyString"
+  end
+end
