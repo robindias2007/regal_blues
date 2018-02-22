8844a871c5bcb58f6f0083175b69a18a150e05be
Robin Dias <robin.dias@amidostech.com>
2018-02-22 18:48:44 +0530

    message count

--- app/serializers/v1/supports/users_serializer.rb
+++ app/serializers/v1/supports/users_serializer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class V1::Supports::UsersSerializer < ActiveModel::Serializer
-  attributes :id, :full_name, :username,  :conversation_id
+  attributes :id, :full_name, :username,  :conversation_id, :message_count
 
   def conversation_id
   	if object.conversations.present?
@@ -11,5 +11,12 @@ class V1::Supports::UsersSerializer < ActiveModel::Serializer
   	end
   end
 
+  def message_count
+  	if object.conversations.present?
+  		object.conversations.first.messages.count
+  	else
+  		0
+  	end
+  end
 end
 
