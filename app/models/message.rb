ee0165692355720fdc395c6e027283f8bd3d2ff6
Robin Dias <robin.dias@amidostech.com>
2018-02-27 18:03:39 +0530

    message change

--- app/models/message.rb
+++ app/models/message.rb
@@ -40,7 +40,7 @@ class Message < ApplicationRecord
 	  			NotificationsMailer.message_notification_support(sup, self, sender, type, type_name, offer_designer_name).deliver_later
 	  			begin
             Message.new.msg_notification(sup.devise_token, self, "You have a new chat message")
-            Message.new.support_msg_notification(sup.devise_token, self, "You have a new chat message from #{sender.full_name} with username #{sender.username}")
+            Message.new.support_msg_notification(sup.devise_token, self, "You have a new chat message from #{sender.full_name} with username #{sender.username}. #{self.body}")
           rescue
 	  			end
 	  		end
