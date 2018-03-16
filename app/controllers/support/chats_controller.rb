c56df7a8b305181ab45ff99115f3c6a2bafc06c1
Robin Dias <robin.dias@amidostech.com>
2018-03-16 17:03:01 +0530

    chat page

--- app/controllers/support/chats_controller.rb
+++ app/controllers/support/chats_controller.rb
@@ -1,7 +1,9 @@
 class Support::ChatsController < ApplicationController
   
-  def chat_details
-  	@chats = SupportChat.all 
+  def index
+    convo_id = Message.pluck(:conversation_id).uniq
+    user_id = Conversation.where(id:convo_id).pluck(:conversationable_id)
+    @users = User.where(id:user_id).order(updated_at: :desc).paginate(:page => params[:page], :per_page => 100)
   end
 
 end
