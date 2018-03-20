29dbcf500721954f38777720484b512a3d124c68
Robin Dias <robin.dias@amidostech.com>
2018-03-20 16:00:03 +0530

    close request changes

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,6 +4,21 @@ class EventsController < ApplicationController
 	def create
 		@event = Event.new(event_params)
 		if @event.save!
+			if @event.event_name == "CLOSE_REQUEST"
+				user = User.find_by(username:@event.username)
+				message_body = "Hi! We noticed that you tried placing a request on our app. In case you are having trouble submitting requests or have some questions, do let us know we are just a chat away! Thank You!"
+				if user.conversations.present?
+		      message = Message.new(body:message_body, conversation_id:user.conversations.first.id, sender_id:Support.first.common_id)
+		      message.save!
+		      user.update(updated_at:DateTime.now)  
+		    else
+		      convo = Conversation.new(receiver_id:Support.first.common_id, receiver_type:"support", conversationable_id:user.id, conversationable_type:"User")   
+		      convo.save!
+		      message = Message.new(body:message_body, conversation_id:convo.id, sender_id:Support.first.common_id)
+		      message.save!
+		      user.update(updated_at:DateTime.now)
+		    end
+			end
       render json: { message: 'Event Updated' }, status: 201
 		else
       render json: { message: 'Event Couldnt Be Updated' }, status: 404
