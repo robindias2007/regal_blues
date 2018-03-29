d4faa9c409119b0afbdfd53d2aa19f6b1ce18a03
Robin Dias <robin.dias@amidostech.com>
2018-03-29 15:55:38 +0530

    scheduler messages change

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -12,12 +12,15 @@ scheduler.cron '0 0 0 * * 1-7 Asia/Kolkata' do
 	if requests.present?
 		requests.each do |f|
 			if (f.created_at + 48.hours).to_date == Date.today
-				unless f.user.conversations.present?
-					convo = Conversation.create(receiver_id:Support.first.common_id, receiver_type:"support", conversationable_id:f.user.id, conversationable_type:"User")		
+				body = "Hi #{f.user.full_name}! In regards to your request #{f.name}, the budget seems low. Request you to kindly increase the same or select another outfit. Please make sure to enter your maximum budget, if your budget is low for the outfit requested and no options are possible then you will not get any offers from designers. Also note that designers will send you fabric options and work based on your budget and what best can be done close to your budget. Thank You!"
+				if f.user.conversations.present?
+	  			message = Message.create(body:body, conversation_id:f.user.conversations.first.id, sender_id:Support.first.common_id)
+	  			f.user.update(updated_at:DateTime.now)
+				else
+					convo = Conversation.create(receiver_id:Support.first.common_id, receiver_type:"support", conversationable_id:f.user.id, conversationable_type:"User")
+					message = Message.create(body:body, conversation_id:convo.id, sender_id:Support.first.common_id)
+	  			f.user.update(updated_at:DateTime.now)
 				end
-				body = "Hi #{f.user.full_name}! As per the designers, the budget is too low for the garment. Request you to kindly increase the same or select another outfit. Please make sure to enter your maximum budget, if your budget is low for the outfit requested and no options are possible then you will not get any offers from designers. Also note that designers will send you fabric options and work based on your budget and what best can be done close to your budget. Thank You!"
-	  		message = Message.create(body:body, conversation_id:f.user.conversations.first.id, sender_id:Support.first.common_id)
-	  		f.user.update(updated_at:DateTime.now)
 			end
 		end
 	end
@@ -31,12 +34,15 @@ scheduler.cron '2 0 0 * * 1-7 Asia/Kolkata' do
 	if orders.present?
 		orders.each do |f|
 			if (f.created_at + 1.day).to_date == Date.today
-				unless f.user.conversations.present?
-					Conversation.create(receiver_id:Support.first.common_id, receiver_type:"support", conversationable_id:f.user.id, conversationable_type:"User")		
-				end
 				body = "Thank You #{f.user.full_name}! We have received your payment. Kindly log in to your app and navigate to “Manage orders” in the “Accounts” to submit your managements."
-	  		message = Message.create(body:body, conversation_id:f.user.conversations.first.id, sender_id:Support.first.common_id)
-	  		f.user.update(updated_at:DateTime.now)
+				if f.user.conversations.present?
+	  			message = Message.create(body:body, conversation_id:f.user.conversations.first.id, sender_id:Support.first.common_id)
+	  			f.user.update(updated_at:DateTime.now)
+				else
+					convo = Conversation.create(receiver_id:Support.first.common_id, receiver_type:"support", conversationable_id:f.user.id, conversationable_type:"User")
+					message = Message.create(body:body, conversation_id:convo.id, sender_id:Support.first.common_id)
+	  			f.user.update(updated_at:DateTime.now)
+				end
 			end
 		end
 	end
@@ -50,12 +56,15 @@ scheduler.cron '4 0 0 * * 1-7 Asia/Kolkata' do
 		requests.each do |f|
 			if (f.offers.first.created_at + 1.day).to_date == Date.today
 				unless f.status == "confirmed"
-					unless f.user.conversations.present?
-						Conversation.create(receiver_id:Support.first.common_id, receiver_type:"support", conversationable_id:f.user.id, conversationable_type:"User")		
+					body = "Hi #{f.user.full_name}! You had received offer(s) for your request #{f.name}. Kindly login to our app, “ Account -> Manage Request” to respond to the offer(s). Do let us know incase you have questions regarding the same or need sketches of outfits or any embroidery/work if applicable. Thank You!"
+					if f.user.conversations.present?
+		  			message = Message.create(body:body, conversation_id:f.user.conversations.first.id, sender_id:Support.first.common_id)
+		  			f.user.update(updated_at:DateTime.now)
+					else
+						convo = Conversation.create(receiver_id:Support.first.common_id, receiver_type:"support", conversationable_id:f.user.id, conversationable_type:"User")
+						message = Message.create(body:body, conversation_id:convo.id, sender_id:Support.first.common_id)
+		  			f.user.update(updated_at:DateTime.now)
 					end
-					body = "Hi #{f.user.full_name}! You have received a new offer for your request. Kindly login to our app, “ Account -> Manage Request” to respond to the offer. Do let us know incase you have questions regarding the same. Thank You!"
-		  		message = Message.create(body:body, conversation_id:f.user.conversations.first.id, sender_id:Support.first.common_id)
-		  		f.user.update(updated_at:DateTime.now)
 		  	end	
 			end
 		end
@@ -70,12 +79,15 @@ scheduler.cron '6 0 0 * * 1-7 Asia/Kolkata' do
 		requests.each do |f|
 			if (f.offers.first.created_at + 3.days).to_date == Date.today
 				unless f.status == "confirmed"
-					unless f.user.conversations.present?
-						Conversation.create(receiver_id:Support.first.common_id, receiver_type:"support", conversationable_id:f.user.id, conversationable_type:"User")		
+					body = "Hi #{f.user.full_name}! You had received offer(s) for your request #{f.name}. Kindly login to our app, “ Account -> Manage Request” to respond to the offer(s). Do let us know incase you have questions regarding the same or need sketches of outfits or any embroidery/work if applicable. Thank You!"
+					if f.user.conversations.present?
+		  			message = Message.create(body:body, conversation_id:f.user.conversations.first.id, sender_id:Support.first.common_id)
+		  			f.user.update(updated_at:DateTime.now)
+					else
+						convo = Conversation.create(receiver_id:Support.first.common_id, receiver_type:"support", conversationable_id:f.user.id, conversationable_type:"User")
+						message = Message.create(body:body, conversation_id:convo.id, sender_id:Support.first.common_id)
+		  			f.user.update(updated_at:DateTime.now)
 					end
-					body = "Hi #{f.user.full_name}! You have received a new offer for your request. Kindly login to our app, “ Account -> Manage Request” to respond to the offer. Do let us know incase you have questions regarding the same. Thank You!"
-		  		message = Message.create(body:body, conversation_id:f.user.conversations.first.id, sender_id:Support.first.common_id)
-		  		f.user.update(updated_at:DateTime.now)
 		  	end	
 			end
 		end
@@ -90,12 +102,15 @@ scheduler.cron '8 0 0 * * 1-7 Asia/Kolkata' do
 		requests.each do |f|
 			if (f.offers.first.created_at + 5.days).to_date == Date.today
 				unless f.status == "confirmed"
-					unless f.user.conversations.present?
-						Conversation.create(receiver_id:Support.first.common_id, receiver_type:"support", conversationable_id:f.user.id, conversationable_type:"User")		
+					body = "Hi #{f.user.full_name}! You had received offer(s) for your request #{f.name}. Kindly login to our app, “ Account -> Manage Request” to respond to the offer(s). Do let us know incase you have questions regarding the same or need sketches of outfits or any embroidery/work if applicable. Thank You!"
+					if f.user.conversations.present?
+		  			message = Message.create(body:body, conversation_id:f.user.conversations.first.id, sender_id:Support.first.common_id)
+		  			f.user.update(updated_at:DateTime.now)
+					else
+						convo = Conversation.create(receiver_id:Support.first.common_id, receiver_type:"support", conversationable_id:f.user.id, conversationable_type:"User")
+						message = Message.create(body:body, conversation_id:convo.id, sender_id:Support.first.common_id)
+		  			f.user.update(updated_at:DateTime.now)
 					end
-					body = "Hi #{f.user.full_name}! You have received a new offer for your request. Kindly login to our app, “ Account -> Manage Request” to respond to the offer. Do let us know incase you have questions regarding the same. Thank You!"
-		  		message = Message.create(body:body, conversation_id:f.user.conversations.first.id, sender_id:Support.first.common_id)
-		  		f.user.update(updated_at:DateTime.now)
 		  	end	
 			end
 		end
